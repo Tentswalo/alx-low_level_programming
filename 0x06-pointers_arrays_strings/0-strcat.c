0143b9a73f681d0d22d6aa216bcc96fd5ee59692
Michelle <mpenane1@gmail.com>
2022-06-16 14:47:16 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,20 @@
 *
 * Return: address of dest
 */
+
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+int i = 0;
+int j = 0;
 
-i = j = 0;
-while (*(dest + i))
+while (dest[i] != '\0')
 i++;
-while ((*(dest + i) = *(src + j)))
+while (src[j] != '\0')
 {
+*(dest + i) = src[j];
 i++;
 j++;
 }
+*(dest + i) = '\0';
 return (dest);
 }
