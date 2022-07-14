f699b95d69fb6c07634d8459e3a8d09966dad089
Michelle <mpenane1@gmail.com>
2022-07-14 13:42:28 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,9 @@
 /**
 * myCleanupFun - prints before main function executed
 * 
+* Return: void
 */
 void myCleanupFun(void) __attribute__ ((constructor));
-
-void myCleanupFun(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
