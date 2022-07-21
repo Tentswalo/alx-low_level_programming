4de47dc2f1395e50681a099b0e7a9a8ccc1be537
Michelle <mpenane1@gmail.com>
2022-07-21 15:42:48 -0700

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _strlen(const char *s);
-unsigned in binary_to_uint(const char *b);
 int _putchar(char c);
+int _strlen(const char *s);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 void _divide(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
