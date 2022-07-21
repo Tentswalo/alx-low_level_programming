ccc523923ff6e53bb2fa27ed1f6b94cbd0f9c57b
Michelle <mpenane1@gmail.com>
2022-07-21 16:15:23 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* flip_bits - ...
+* @n: ...
+* @m: ...
+*
+* Return: ...
+*/
+unsigned in flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int mask = 0, count = 0;
+
+mask = n ^ m;
+while (mask)
+{
+if (mask & 1)
+count++;
+
+mask >>= 1;
+}
+return (count);
+}
