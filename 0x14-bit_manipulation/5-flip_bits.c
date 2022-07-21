6ee1f00b2914d1b2b5c13aadc37b9722903c33b7
Michelle <mpenane1@gmail.com>
2022-07-21 16:25:34 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,7 +7,7 @@
 *
 * Return: number of bits
 */
-unsigned in flip_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned long int diff, check;
 unsigned int count, i;
