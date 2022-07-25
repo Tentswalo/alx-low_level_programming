20844308d5648ce9e30812a4b0d6259ce9bf4fdc
Michelle <mpenane1@gmail.com>
2022-07-25 12:31:28 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * read_textfile - Entry point
- * @filename: file name
- * @letters: size
- * Return: always 0
- */
+* read_textfile - Entry point
+* @filename: file name
+* @letters: size
+* Return: always 0
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int file, rd, wr;
-    char *buf;
+int file, rd, wr;
+char *buf;
 
-    if (filename == NULL)
-      return (0);
+if (filename == NULL)
+return (0);
 
-    file = open(filename, O_RDONLY);
+file = open(filename, O_RDONLY);
 
-    if (file == -1)
-      return (0);
+if (file == -1)
+return (0);
 
-    buf = malloc(sizeof(char) * letters + 1);
-    if (buf == NULL)
-      return (0);
+buf = malloc(sizeof(char) * letters + 1);
+if (buf == NULL)
+return (0);
 
-    rd = read(file, buf, letters);
-    if (rd == -1)
-      return (0);
+rd = read(file, buf, letters);
+if (rd == -1)
+return (0);
 
-    buf[letters] = '\0';
+buf[letters] = '\0';
 
-    wr = write(1, buf, rd);
-    if (wr == -1)
-      return (0);
+wr = write(1, buf, rd);
+if (wr == -1)
+return (0);
 
-    close(file);
-    free(buf);
-    return (wr);
+close(file);
+free(buf);
+return (wr);
 }
