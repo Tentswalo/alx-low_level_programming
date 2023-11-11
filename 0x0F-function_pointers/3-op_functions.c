14d144752a0e885bb9c465e5f83140c929041fe9
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2023-11-11 03:20:07 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,66 +1,53 @@
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
-* op_add - addition operation
-* @a: first term
-* @b: second term
-*
-* Return: result
-*/
-
+ * op_add - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
-
 /**
-* op_sub - subtraction operation
-* @a: first term
-* @b: second term
-*
-* Return: result
-*/
-
+ * op_sub - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
-
 /**
-* op_mul - multiplication operation
-* @a: first term
-* @b: second term
-*
-* Return: result
-*/
-
+ * op_mul - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
-
 /**
-* op_div - division operation
-* @a: first term
-* @b: second term
-*
-* Return: result
-*/
-
+ * op_div - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_div(int a, int b)
 {
-return (a / b);
+	return (a / b);
 }
-
 /**
-* op_mod - modulo operation
-* @a: first term
-* @b: second term
-*
-* Return: result
-*/
-
+ * op_mod - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_mod(int a, int b)
 {
-return (a % b);
+	return (a % b);
 }
