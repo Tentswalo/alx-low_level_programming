5338498122d9fdcc712f073cd3932fabf6844ec0
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2023-11-11 03:22:50 +0200

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,25 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
-* main - Entry point
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: Always 0 (success)
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
 */
-
 int main(int argc, char *argv[])
 {
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-if (atoi(argv[1]) < 0)
-{
-printf("Error\n");
-exit(2);
-}
-return (0);
+	int i, n;
+
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
 }
