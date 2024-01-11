5f53ab2513196aba8eac57b82d2ceb6b134cc019
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2024-01-12 00:15:03 +0200

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,29 @@
 #include "lists.h"
+
 /**
- * dlistint_len - Return number of elements
- * @h: Pointer to struct
- * Return: Amount of elements dll
+ * dlistint_len - Number of ll
+ *
+ * @h: Head
+ *
+ * Return: Number of elements
  */
+
 size_t dlistint_len(const dlistint_t *h)
 {
-  int count_node = 0;
+	unsigned int count = 0;
+	const dlistint_t *list;
 
-  while (h != NULL)
-  {
-    h = h->next;
-    count_node++;
-  }
-  return (count_node);
+	list = malloc(sizeof(dlistint_t));
+	list = h;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+	while (list)
+	{
+		list = list->next;
+		count++;
+	}
+	return (count);
 }
